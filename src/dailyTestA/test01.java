4cb7a29ee991812605823b6eaf6255ddb1698524
boooobea <childhop90@gmail.com>
2022-05-05 17:52:14 +0900

    두 수 비교하기 1330

--- src/dailyTestA/test01.java
+++ src/dailyTestA/test01.java
@@ -1,10 +1,10 @@
 package dailyTestA;
 import java.util.Scanner;
 
+//두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오 
 public class test01 {
 
 	public static void main(String[]args) {
-//두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오 
 		
 	Scanner sc = new Scanner(System.in);
 	System.out.println("두 정수를 입력하세요.");
