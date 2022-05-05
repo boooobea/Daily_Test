3240641ffaa131d95a8ccf6a1e73e70baac0b4fe
boooobea <childhop90@gmail.com>
2022-05-05 18:02:18 +0900

    시험 성적 9498

--- src/dailyTestA/test02.java
+++ src/dailyTestA/test02.java
@@ -1,17 +1,17 @@
 package dailyTestA;
 
+//시험점수를 입력받아 90~100점은 A, 80~89점은 B, 70~79점은 C, 60~69점은 D, 나머지 점수는 F를
+//출력하는 프로그램을 작성하시오
 import java.util.Scanner;
 
 public class test02 {
-//시험점수를 입력받아 90~100점은 A, 80~89점은 B, 70~79점은 C, 60~69점은 D, 나머지 점수는 F를
-//출력하는 프로그램을 작성하시오
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("점수를 입력하세요.");
 		int score = scanner.nextInt();
 
-		if(score >=90 ) {
+		if(score >=90) {
 			System.out.println("A");
 		} else if (score>=80) {
 			System.out.println("B");
@@ -20,7 +20,7 @@ public class test02 {
 		} else if (score>=60) {
 			System.out.println("D");
 		} else {
-			System.out.println("E");
+			System.out.println("F");
 		}//if-else
 		   
 	}//main
