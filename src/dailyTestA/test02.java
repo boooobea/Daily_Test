16788451b4a97873968cc9ac9e59d4bfa5c95002
bobae <childhop90@gmail.com>
2022-05-06 22:49:18 +0900

    조건문 9498 시험성적

--- src/dailyTestA/test02.java
+++ src/dailyTestA/test02.java
@@ -1,9 +1,10 @@
 package dailyTestA;
 
-//시험점수를 입력받아 90~100점은 A, 80~89점은 B, 70~79점은 C, 60~69점은 D, 나머지 점수는 F를
-//출력하는 프로그램을 작성하시오
 import java.util.Scanner;
+//조건문 9498 시험 성적
 
+//시험점수를 입력받아 90~100점은 A, 80~89점은 B, 70~79점은 C, 60~69점은 D, 
+//나머지 점수는 F를 출력하는 프로그램을 작성하시오
 public class test02 {
 	public static void main(String[] args) {
 		
