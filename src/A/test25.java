794faa96a9a8430d3defc67ac9e7c719db3b6723
bobae <childhop90@gmail.com>
2022-05-08 18:18:40 +0900

    사칙연산 10869

---
+++ src/A/test25.java
@@ -0,0 +1,22 @@
+package A;
+
+import java.util.Scanner;
+
+//사칙연산 10869
+//두 자연수 A와 B가 주어진다. 
+//이때, A+B, A-B, A*B, A/B(몫), A%B(나머지)를 출력하는 프로그램을 작성하시오. 
+public class test25 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+	}//main
+}//end class
