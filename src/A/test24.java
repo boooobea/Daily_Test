f28f33b2931a5242c56a1aafd4606b10ec2f62ef
bobae <childhop90@gmail.com>
2022-05-08 18:18:34 +0900

    연산 1008

---
+++ src/A/test24.java
@@ -0,0 +1,17 @@
+package A;
+
+import java.util.Scanner;
+
+//1008
+//두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+public class test24 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println((double)a/b);
+	}//main
+}//end class
