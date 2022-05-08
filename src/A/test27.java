3ea66b590deafdaf47c1923218229a7ae4b8c7d7
bobae <childhop90@gmail.com>
2022-05-08 18:19:02 +0900

    18108

---
+++ src/A/test27.java
@@ -0,0 +1,16 @@
+package A;
+
+import java.util.Scanner;
+
+//18108 
+/* */
+public class test27 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		int a = 543;
+		
+		System.out.println(year-a);
+	}//main
+}//end class
