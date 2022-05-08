ee91a9eed68065e2583809932e7748ab06171f3d
bobae <childhop90@gmail.com>
2022-05-08 18:18:05 +0900

    연산 1000 a+b

---
+++ src/A/test21.java
@@ -0,0 +1,19 @@
+package A;
+
+import java.util.Scanner;
+
+//1000 a+b
+//두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+public class test21 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.println(a+b);
+		
+	}//main
+}//end class
