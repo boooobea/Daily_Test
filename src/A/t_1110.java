f2572f70eab356c489257828aaa147412da75311
bobae <childhop90@gmail.com>
2022-05-13 01:00:01 +0900

    반복문 1110 더하기싸이클

---
+++ src/A/t_1110.java
@@ -0,0 +1,28 @@
+package A;
+
+import java.util.Scanner;
+
+public class t_1110 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int copy = num;
+		
+		int count = 0;
+		
+		while(true) {
+			num = ((num%10)*10)+(((num%10)+(num/10))%10);
+			count ++;
+			
+			if(copy == num) {
+				break;
+			}
+		}//for:사이클 횟수 체크
+				
+		System.out.println(count);
+	}//main
+	
+}//end class
