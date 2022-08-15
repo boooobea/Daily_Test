dbaf50ece5ff9d160ae50ad5403ebd5a92a5f669
bobae <childhop90@gmail.com>
2022-08-16 00:53:21 +0900

    백준 1259 팰린드롬수

--- src/B/t_1259.java
+++ src/B/t_1259.java
@@ -9,19 +9,20 @@ public class t_1259 {
 	public static void main(String[] args) throws IOException{
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+		//Scanner 보다 buffer 사용하기
+		
 		while (true) {
 
 			String N = br.readLine();
 
-			if (N.equals("0")) break;
+			if (N.equals("0")) break;		//0을만나면 break;탈출 
 
 			
 			boolean result = true;
 
 			for (int i = 0; i < N.length(); i++) {
 
-				if (N.charAt(i) != N.charAt(N.length() - i - 1)) {
+				if (N.charAt(i) != N.charAt(N.length() - i - 1)) { //index Reverse
 					result = false;
 					break;
 				}
